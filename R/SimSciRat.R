3cf05cae0ed2982d2e17633c33084af3cb9d7b1a
Mario Hasler <hasler@email.uni-kiel.de>
2008-10-23 00:00:00 +0000

    version 1.0

---
+++ R/SimSciRat.R
@@ -0,0 +1,126 @@
+`SimSciRat` <-
+function(data,grp,resp=NULL,type="Dunnett",base=1,Num.Contrast=NULL,Den.Contrast=NULL,
+                      alternative="two.sided",covar.equal=FALSE,conf.level=0.95) {
+
+
+alternative <- match.arg(alternative, choices=c("two.sided", "less", "greater"))
+if (length(grp) > 1) {
+  stop("Specify only one grouping variable")
+}
+tr.names <- levels(data[,grp])
+ntr <- length(levels(data[,grp]))                                        # number of treatments
+trlist <- split(x=data,f=data[,grp])                                     # data list splitted into the different treatments
+ssvec <- numeric(ntr)                                                    # sample sizes for the treatments
+for (i in 1: ntr) {
+  ssvec[i] <- nrow(trlist[[i]])
+}
+
+if (is.null(resp)) {
+  resp <- names(data[,-which(names(data)%in%grp),drop=FALSE])
+} 
+nep <- length(resp)                                                      # number of endpoints
+
+if (covar.equal==TRUE) {
+  if ( sum(ssvec-1)<nep ) {
+    stop("Not enough observations or too many endpoints to be analyzed")
+  }
+} else {
+  if ( any((ssvec-1)<nep) ) {
+    stop("Not enough observations or too many endpoints to be analyzed")
+  }
+}
+
+for (i in 1:ntr) {
+  trlist[[i]] <- as.matrix(trlist[[i]][,resp])                           # data list without factor
+}
+
+for (i in 1:ntr) {
+  if (is.numeric(trlist[[i]])==FALSE) {
+    stop("Response variables must be numeric")
+  }
+  if (any(is.na(trlist[[i]]))) {
+    stop("No equal sample sizes for the endpoints; missing values")
+  }
+}
+
+if (is.null(Num.Contrast)==FALSE || is.null(Num.Contrast)==FALSE) {
+  if (is.null(Den.Contrast==TRUE) && is.null(Den.Contrast==FALSE)) {
+    stop("Num.Contrast is specified, but Den.Contrast is missing")
+  }
+  if (is.null(Den.Contrast==FALSE) && is.null(Den.Contrast==TRUE)) {
+    stop("Den.Contrast is specified, but Num.Contrast is missing")
+  }
+  if (is.null(Den.Contrast)==FALSE && is.null(Num.Contrast)==FALSE) {
+    if (nrow(Den.Contrast)!=nrow(Num.Contrast)) {
+      stop("Number of rows in Num.Contrast and Den.Contrast should be the same")
+    }
+    if (ncol(Den.Contrast)!=ntr || ncol(Num.Contrast)!=ntr) {
+      stop("Number of columns in Num.Contrast or Den.Contrast should be the same as number of groups")
+    }
+    NC0 <- apply(X=Num.Contrast, MARGIN=1, function(x) {
+      all(x==0)
+    })
+    DC0 <- apply(X=Den.Contrast, MARGIN=1, function(x) {
+      all(x==0)
+    })
+    if (any(c(NC0, DC0))) {
+      cat("Warning: At least one row of the numerator or denominator contrast matrices is a vector with all components", "\n",
+          "equal to zero", "\n")
+    }
+    Num.Cmat <- Num.Contrast
+    Den.Cmat <- Den.Contrast
+    type <- "User defined"
+    if (is.null(rownames(Num.Cmat)) && is.null(rownames(Den.Cmat))) {
+      comp.names <- paste("C", 1:nrow(Num.Cmat), sep="")
+    } else {
+      if (any(rownames(Num.Cmat)!=rownames(Den.Cmat))) {
+        comp.names <- paste(rownames(Num.Cmat), rownames(Den.Cmat), 
+                       sep="/")
+      } else {
+        comp.names <- rownames(Num.Cmat)
+      }
+    }
+  }
+} else {
+  type <- match.arg(type, choices=c("Dunnett", "Tukey", "Sequen", "AVE", "GrandMean", "Changepoint", "Marcus", 
+    "McDermott", "Williams", "UmbrellaWilliams"))
+  names(ssvec) <- tr.names
+  Cmat <- contrMatRatio(n=ssvec, type=type, base=base)
+  Num.Cmat <- Cmat$numC
+  Den.Cmat <- Cmat$denC
+  comp.names <- Cmat$rnames
+}
+
+if (length(conf.level)>1 || !is.numeric(conf.level) || conf.level>=1 || conf.level<=0 ) {
+  stop("conf.level must be a single numeric value between 0 and 1")
+}
+
+if (covar.equal==TRUE) {
+  out <- SimSciRatHom(trlist=trlist, grp=grp, ntr=ntr, nep=nep, ssvec=ssvec, Num.Contrast=Num.Cmat, Den.Contrast=Den.Cmat,
+                      alternative=alternative, conf.level=conf.level)
+} else {
+  out <- SimSciRatHet(trlist=trlist, grp=grp, ntr=ntr, nep=nep, ssvec=ssvec, Num.Contrast=Num.Cmat, Den.Contrast=Den.Cmat,
+                      alternative=alternative, conf.level=conf.level)
+}
+out$type <- type
+out$test.class <- "ratios"
+out$covar.equal <- covar.equal
+out$comp.names <- comp.names
+out$resp <- resp
+colnames(out$estimate) <- resp
+if (type=="User defined") {
+  rownames(out$estimate) <- comp.names
+  rownames(out$Num.Contrast) <- comp.names; colnames(out$Num.Contrast) <- tr.names
+  rownames(out$Den.Contrast) <- comp.names; colnames(out$Den.Contrast) <- tr.names
+}
+rownames(out$lower) <- comp.names; colnames(out$lower) <- resp
+rownames(out$upper) <- comp.names; colnames(out$upper) <- resp
+if (covar.equal==FALSE) {
+  rownames(out$degr.fr) <- comp.names; colnames(out$degr.fr) <- resp
+  names(out$CorrMatDat) <- tr.names
+}
+class(out) <- "SimSci"
+return(out)
+
+}
+
---
+++ R/SimTestRatHet.R
@@ -0,0 +1,62 @@
+`SimTestRatHet` <-
+function(trlist, grp, ntr, nep, ssvec, Num.Contrast, Den.Contrast, alternative, Margin) {
+
+
+CMat <- Num.Contrast
+DMat <- Den.Contrast
+ncomp <- nrow(CMat)                                                      # number of comparisons
+
+meanmat <- varmat <- matrix(nrow=ntr, ncol=nep)
+for (i in 1:ntr) { for (j in 1:nep) {
+  meanmat[i,j]=mean(trlist[[i]][,j]); varmat[i,j]=var(trlist[[i]][,j]) }}
+estimate <- CMat%*%meanmat/(DMat%*%meanmat)
+
+defrmat <- matrix(nrow=ncomp, ncol=nep)
+for (j in 1:nep) { for (z in 1:ncomp) {
+defrmat[z,j]=( (sum((CMat[z,]-Margin[z,j]*DMat[z,])^2*varmat[,j]/ssvec))^2 ) / 
+             sum( ( (CMat[z,]-Margin[z,j]*DMat[z,])^4*varmat[,j]^2 ) / ( ssvec^2*(ssvec-1) ) ) }}
+defrmat[defrmat<2] <- 2                                                  # to be well-defined
+
+CovMatDat <- CorrMatDat <- list()                                        # list of covariance/ correlation matrices of the data
+for (i in 1:ntr) { CovMatDat[[i]]  <- cov(trlist[[i]])
+                   CorrMatDat[[i]] <- cov2cor(CovMatDat[[i]]) }
+
+M <- diag(1/ssvec)
+R <- NULL
+for (z in 1:ncomp) {
+  Rrow <- NULL
+  for (w in 1:ncomp) {
+    Rpart <- matrix(nrow=nep,ncol=nep)
+    for (i in 1:nep) { for (h in 1:nep) {
+      Rpart[i,h]=( t(CMat[z,]-Margin[z,i]*DMat[z,])%*%
+                   diag(unlist( lapply( X=CovMatDat,FUN=function(x){x[i,h]} ) ))%*%M%*%(CMat[w,]-Margin[w,h]*DMat[w,]) ) /
+                 sqrt( ( t(CMat[z,]-Margin[z,i]*DMat[z,])%*%diag(varmat[,i])%*%M%*%(CMat[z,]-Margin[z,i]*DMat[z,]) ) *
+                       ( t(CMat[w,]-Margin[w,h]*DMat[w,])%*%diag(varmat[,h])%*%M%*%(CMat[w,]-Margin[w,h]*DMat[w,]) ) ) }
+    }
+    Rrow <- cbind(Rrow,Rpart)
+  }
+  R <- rbind(R, Rrow)                                                    # correlation matrix for test.stat
+}
+diag(R)=rep(1,times=nrow(R))
+
+test.stat <- p.val.adj <- p.val.raw <- matrix(nrow=ncomp, ncol=nep)      # matrices of test statistics and p.vals
+for (z in 1:ncomp) { for (i in 1:nep) {
+  test.stat[z,i]=( t(CMat[z,]-Margin[z,i]*DMat[z,])%*%meanmat[,i] ) /
+                 sqrt( t(CMat[z,]-Margin[z,i]*DMat[z,])%*%diag(varmat[,i])%*%M%*%(CMat[z,]-Margin[z,i]*DMat[z,]) )
+  if (alternative=="greater") {
+    p.val.adj[z,i]=1-pmvt(lower=-Inf,upper=rep(test.stat[z,i],times=ncomp*nep),df=defrmat[z,i],corr=R)[1]
+    p.val.raw[z,i]=pt(q=test.stat[z,i],df=defrmat[z,i],lower.tail=FALSE) }
+  if (alternative=="less") {
+    p.val.adj[z,i]=1-pmvt(lower=rep(test.stat[z,i],times=ncomp*nep),upper=Inf,df=defrmat[z,i],corr=R)[1]
+    p.val.raw[z,i]=pt(q=test.stat[z,i],df=defrmat[z,i],lower.tail=TRUE) }
+  if (alternative=="two.sided") {
+    p.val.adj[z,i]=min((1-pmvt(lower=-Inf,upper=rep(abs(test.stat[z,i]),times=ncomp*nep),df=defrmat[z,i],corr=R)[1])*2,1)
+    p.val.raw[z,i]=min(pt(q=abs(test.stat[z,i]),df=defrmat[z,i],lower.tail=FALSE)*2,1) }
+}}
+
+list(estimate=estimate, statistic=test.stat, p.val.raw=p.val.raw, p.val.adj=p.val.adj,
+     CorrMatDat=CorrMatDat, CorrMatComp=R, degr.fr=defrmat,
+     Num.Contrast=CMat, Den.Contrast=DMat, Margin=Margin, alternative=alternative)
+
+}
+
